b793762a0230acf135510ed3ad1f3b4701d36442
Kaiying Fu <kaiying.fu@gmail.com>
2020-08-01 14:47:44 +0800

    Add root to launch page

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Define default URL options
+  config.action_mailer.default_url_options = { host: 'https://rep-er-toire.herokuapp.com' }
 end
