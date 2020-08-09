92ee8577e82eed5a4e17a7f8d410dc6bf1df64e7
Kaiying Fu <kaiying.fu@gmail.com>
2020-08-09 23:03:58 +0800

    Add create story functionality

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,7 +2,25 @@ class StoriesController < ApplicationController
   def new
     @story = Story.new
   end
+
   def index
     @stories = Story.all
+  end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to story_path(@story)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @story = Story.find_by_id(params[:id])
+  end
+
+  def story_params
+    params.require(:story).permit(:story_id, :title, :photos, :latitude, :longitude, :price_rating, :worth_it, :mood_type, :experience_type, :ambience_type)
   end
 end
