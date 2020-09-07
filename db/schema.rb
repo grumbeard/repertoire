ab363c2b57fcbffa0b3e5edf9d855580278f194b
Kaiying Fu <kaiying.fu@gmail.com>
2020-09-07 22:19:57 +0800

    Add Address to Story

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_090641) do
+ActiveRecord::Schema.define(version: 2020_09_07_141909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2020_09_06_090641) do
     t.string "ambience_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
   create_table "tag_categories", force: :cascade do |t|
