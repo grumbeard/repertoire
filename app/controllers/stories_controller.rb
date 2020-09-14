6a006b0a7f3eb6eb48bbacca55924b6710071b5e
Kaiying Fu <kaiying.fu@gmail.com>
2020-09-15 00:06:04 +0800

    Add location tagging to story

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -22,9 +22,10 @@ class StoriesController < ApplicationController
       address_details << results.first.neighbourhood if results.first.neighbourhood
       address_details << results.first.suburb if results.first.suburb
       address_details.each do |value|
-        "Tagging with Location: #{value}"
         tag = Tag.new(name: value, tag_category: location) unless Tag.where(name: value).present?
         tag.save if tag
+        tagging = Tagging.new(story: story, tag: Tag.where(name: value).first)
+        tagging.save if tagging
       end
     end
     if @story.save
